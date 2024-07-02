f6de5b7fc36e39d0acec40f3005a4fc3bdb2b56d
Awssam Saidi <awtsoft@gmail.com>
2024-07-02 03:38:11 +0100

    Creating landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="landing-div">
+        <p>Welcome to </p>
+        <h1>Paradise nursery</h1>
+        <p>Your go-to place for beautiful houseplants. Discover a wide variety of plants to bring life and color into your home. Whether you’re a seasoned plant parent or just starting, we have the perfect plants for you. Explore our collection and find your new green companions today.</p>
+
+        <button >Get Started</button>
+
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
